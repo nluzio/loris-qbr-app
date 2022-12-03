560796cb2762d69ac423a2b88170794e4c5389f9
nluzio <nicholasluzio@gmail.com>
2022-12-03 15:14:51 -0500

    Added randomly generated dataframe as demo data. Updated Change section with metrics instead of a table.
    Added some additional functions to clean flow

--- main.py
+++ main.py
@@ -16,10 +16,9 @@ st.set_page_config(layout='wide')
 # define some functions
 @st.cache
 def data_loader(first_data):
-    if first_data != '':
-        data = pd.read_csv(first_data)
-        data['date'] = pd.to_datetime(data['CONV_CREATED'])
-        return data
+    data = pd.read_csv(first_data)
+    data['date'] = pd.to_datetime(data['CONV_CREATED'])
+    return data
 
 
 @st.cache
@@ -30,8 +29,9 @@ def make_str(text):
 
 @st.cache
 def date_splitter(data_set, splitter):
-    pre = data_set[data_set['date'] <= splitter].copy()
-    post = data_set[data_set['date'] > splitter].copy()
+    splitter1 = pd.to_datetime(splitter)
+    pre = data_set[data_set['date'] < splitter1].copy()
+    post = data_set[data_set['date'] > splitter1].copy()
     dset = [pre, post]
     return dset
 
@@ -41,19 +41,41 @@ def red_or_green(val):
     return f'background-color: {color}'
 
 
+def random_dates(start, end, n=10):
+    start_u = start.value // 10 ** 9
+    end_u = end.value // 10 ** 9
+
+    return pd.to_datetime(np.random.randint(start_u, end_u, n), unit='s')
+
+
+def make_metric(name, val='N/A', change='N/A', d_color='normal'):
+    st.metric(name, val, delta=change, delta_color=d_color)
+
+
 # Building the streamlit app features
 # title of the app
 st.title('qbr dashboard ')
 
 # get the data and load it
-data_file = st.file_uploader('Choose your data file')
-data = data_loader(data_file)
-clean_data = data.shape[0]
-
+if st.checkbox('Use your own data?'):
+    data_file = st.file_uploader('Choose your data file. Ignore the error, it will go away once you upload a file. I '
+                                 'will fix it soon. :)')
+    data = data_loader(data_file)
+    clean_data = data.shape[0]
+else:
+    st.write('*REMINDER* This is randomly generated data. To use your own dataset, use the checkbox above')
+    data_file = pd.DataFrame(np.random.randint(300, 10000, size=(5000, 6)), columns=['CONVERSATION_ID', 'CONV_ART',
+                                                                                     'CONV_FRT', 'CONV_DURATION',
+                                                                                     'CSAT_SCORE',
+                                                                                     'AGENT_NAME'])
+    rand_dates = pd.DataFrame(
+        random_dates(start=pd.to_datetime('2022-07-01'), end=pd.to_datetime('2022-10-01'), n=5000))
+    data = rand_dates.join(data_file)
+    data.rename(columns={0: 'date'}, inplace=True)
+    clean_data = data.shape[0]
 
 # sidebar build
 with st.sidebar:
-    selection = st.multiselect('Select Which Metrics To Run:', ['CONV_ART', 'CONV_FRT', 'CONV_DURATION', 'CSAT_SCORE'])
     trimmed_convos = 0.0
     if st.checkbox('Trim Outliers?'):
         art_trimmer = st.number_input('Trim ART Samples (seconds):')
@@ -119,14 +141,16 @@ with st.container():
     st.header('Pre-Post Analysis')
     date_break = st.date_input('Date for Pre-Post Analysis. Make sure your period grouping matches the day of your '
                                'break for cleanest results. If your break is a monday, make your period start on '
-                               'monday too')
+                               'monday too', value=pd.to_datetime('2022-08-14'))
     date_break = pd.to_datetime(date_break)
     period = st.text_input('Period for Analysis. Use M or W, the default period start is Sunday. You can specify the '
                            'day you want your grouping to start '
-                           'on by using w-MON, w-TUE.')
+                           'on by using w-MON, w-TUE.', value='W')
     pre_post = date_splitter(data, date_break)
     st.write('Date Range = ', data['date'].min(), ' - ', data['date'].max())
     with st.container():
+        selection = st.multiselect('Select Which Metrics To Run:',
+                                   ['CONV_ART', 'CONV_FRT', 'CONV_DURATION', 'CSAT_SCORE'])
         st.subheader('Average Based Change Analysis')
         Pre, Post = st.columns(2)
         with Pre:
@@ -147,10 +171,21 @@ with st.container():
             val_post = post_data[selection].mean().reset_index().set_index('index')
             val_post.rename(columns={0: 'Overall Average'}, inplace=True)
             st.table(val_post)
-        st.subheader('Percent Change Efficency')
-        matrix = pd.DataFrame()
-        matrix['Percent Change'] = ((val_post - val_pre) / val_pre) * 100
-        st.table(matrix)
+        st.subheader('Percent Change')
+        CONV_ART, CONV_FRT, CONV_DURATION, CSAT_SCORE = st.columns(4)
+        with CONV_ART:
+            delta = ((post_data['CONV_ART'].mean() - pre_data['CONV_ART'].mean()) / pre_data['CONV_ART'].mean()) * 100
+            make_metric(name='Average ART', val=post_data['CONV_ART'].mean(), change=delta, d_color='inverse')
+        with CONV_FRT:
+            delta = ((post_data['CONV_FRT'].mean() - pre_data['CONV_FRT'].mean()) / pre_data['CONV_FRT'].mean()) * 100
+            make_metric(name='Average FRT', val=post_data['CONV_FRT'].mean(), change=delta, d_color='inverse')
+        with CONV_DURATION:
+            delta = ((post_data['CONV_DURATION'].mean() - pre_data['CONV_DURATION'].mean()) / pre_data['CONV_DURATION'].mean()) * 100
+            make_metric(name='Average Duration', val=post_data['CONV_DURATION'].mean(), change=delta, d_color='inverse')
+        with CSAT_SCORE:
+            delta = ((post_data['CSAT_SCORE'].mean() - pre_data['CSAT_SCORE'].mean()) / pre_data['CSAT_SCORE'].mean()) * 100
+            make_metric(name='Average CSAT', val=post_data['CSAT_SCORE'].mean(), change=delta, d_color='normal')
+
 
 with st.container():
-     st.header('Sentiment Target Classification')
+    st.header('Sentiment Target Classification')
