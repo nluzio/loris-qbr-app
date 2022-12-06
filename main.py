d1748184323bf1d64683fd11ea6eb2c817091b5e
Nick Luzio <nicholasluzio@gmail.com>
2022-12-05 21:40:00 -0500

    Added qbr_functions module -- holds all custom functions the dashboard is using. Moving it there to keep the main file cleaner.
    Updates:
    added usage based analysis, team based analysis for high use group, and Sentiment Target Classification visualiztion.
    
    Todo:
    add low usage team based analysis (basically just copy what is done for high use)
    Update all stats with an Individual based usage statistic. Need to continue doing research on which formula would suit our needs here.

--- main.py
+++ main.py
@@ -1,55 +1,16 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import qbr_functions as qbr
 import plotly.express as px
 
 st.set_page_config(layout='wide')
-
 
 # First - define the data set we are working with. FUTURE: this will be an argument passed from the terminal by the user
 # using arg parse
 
 # '/Users/nickluzio/PycharmProjects/Auctane QBR/auctane QA (4).csv'
 # C:\Users\Nick Luzio\Downloads\auctane QA (4).csv
-
-
-# define some functions
-@st.cache
-def data_loader(first_data):
-    data = pd.read_csv(first_data)
-    data['date'] = pd.to_datetime(data['CONV_CREATED'])
-    return data
-
-
-@st.cache
-def make_str(text):
-    txt = str(text)
-    return txt
-
-
-@st.cache
-def date_splitter(data_set, splitter):
-    splitter1 = pd.to_datetime(splitter)
-    pre = data_set[data_set['date'] < splitter1].copy()
-    post = data_set[data_set['date'] > splitter1].copy()
-    dset = [pre, post]
-    return dset
-
-
-def red_or_green(val):
-    color = 'red' if val > 0 else 'green'
-    return f'background-color: {color}'
-
-
-def random_dates(start, end, n=10):
-    start_u = start.value // 10 ** 9
-    end_u = end.value // 10 ** 9
-
-    return pd.to_datetime(np.random.randint(start_u, end_u, n), unit='s')
-
-
-def make_metric(name, val='N/A', change='N/A', d_color='normal'):
-    st.metric(name, val, delta=change, delta_color=d_color)
 
 
 # Building the streamlit app features
@@ -60,7 +21,7 @@ st.title('qbr dashboard ')
 if st.checkbox('Use your own data?'):
     data_file = st.file_uploader('Choose your data file. Ignore the error, it will go away once you upload a file. I '
                                  'will fix it soon. :)')
-    data = data_loader(data_file)
+    data = qbr.data_loader(data_file)
     clean_data = data.shape[0]
 else:
     st.write('*REMINDER* This is randomly generated data. To use your own dataset, use the checkbox above')
@@ -69,7 +30,7 @@ else:
                                                                                      'CSAT_SCORE',
                                                                                      'AGENT_NAME'])
     rand_dates = pd.DataFrame(
-        random_dates(start=pd.to_datetime('2022-07-01'), end=pd.to_datetime('2022-10-01'), n=5000))
+        qbr.random_dates(start=pd.to_datetime('2022-07-01'), end=pd.to_datetime('2022-10-01'), n=5000))
     data = rand_dates.join(data_file)
     data.rename(columns={0: 'date'}, inplace=True)
     clean_data = data.shape[0]
@@ -96,8 +57,6 @@ with st.sidebar:
             trimmed_dur = data[data['CONV_DURATION'] > dur_trimmer].shape[0]
             trimmed_convos = trimmed_convos + trimmed_dur
             data = dur_data.copy()
-
-    usage_breakpoint = st.number_input('Usage % Slicer:')
 
 # Body of app
 
@@ -126,66 +85,158 @@ with st.container():
     ART, FRT, Duration = st.columns(3, gap='large')
     with ART:
         st.subheader('Distribution of ART')
-        scatter = px.scatter(data, x='CONVERSATION_ID', y='CONV_ART', width=600)
+        scatter = px.scatter(data, x='usage', y='CONV_ART', width=600)
         st.plotly_chart(scatter)
     with FRT:
         st.subheader('Distribution of FRT')
-        scatter2 = px.scatter(data, x='CONVERSATION_ID', y='CONV_FRT', width=600)
+        scatter2 = px.scatter(data, x='usage', y='CONV_FRT', width=600)
         st.plotly_chart(scatter2)
     with Duration:
         st.subheader('Distribution of Duration')
-        scatter3 = px.scatter(data, x='CONVERSATION_ID', y='CONV_DURATION', width=600)
+        scatter3 = px.scatter(data, x='usage', y='CONV_DURATION', width=600)
         st.plotly_chart(scatter3)
-
-with st.container():
-    st.header('Pre-Post Analysis')
-    date_break = st.date_input('Date for Pre-Post Analysis. Make sure your period grouping matches the day of your '
-                               'break for cleanest results. If your break is a monday, make your period start on '
-                               'monday too', value=pd.to_datetime('2022-08-14'))
-    date_break = pd.to_datetime(date_break)
-    period = st.text_input('Period for Analysis. Use M or W, the default period start is Sunday. You can specify the '
-                           'day you want your grouping to start '
-                           'on by using w-MON, w-TUE.', value='W')
-    pre_post = date_splitter(data, date_break)
-    st.write('Date Range = ', data['date'].min(), ' - ', data['date'].max())
+if st.checkbox('Usage Based Analysis'):
     with st.container():
-        selection = st.multiselect('Select Which Metrics To Run:',
-                                   ['CONV_ART', 'CONV_FRT', 'CONV_DURATION', 'CSAT_SCORE'])
-        st.subheader('Average Based Change Analysis')
-        Pre, Post = st.columns(2)
-        with Pre:
-            pre_data = pre_post[0]
-            st.subheader('Pre-Date Statistics')
-            pre_stats = pre_data.groupby(pd.Grouper(key='date', freq=period))[selection].mean().reset_index().set_index(
-                'date')
-            st.table(pre_stats)
-            val_pre = pre_data[selection].mean().reset_index().set_index('index')
-            val_pre.rename(columns={0: 'Overall Average'}, inplace=True)
-            st.table(val_pre)
-        with Post:
-            post_data = pre_post[1]
-            st.subheader('Post-Date Statistics')
-            post_stats = post_data.groupby(pd.Grouper(key='date', freq=period))[
-                selection].mean().reset_index().set_index('date')
-            st.table(post_stats)
-            val_post = post_data[selection].mean().reset_index().set_index('index')
-            val_post.rename(columns={0: 'Overall Average'}, inplace=True)
-            st.table(val_post)
-        st.subheader('Percent Change')
-        CONV_ART, CONV_FRT, CONV_DURATION, CSAT_SCORE = st.columns(4)
-        with CONV_ART:
-            delta = ((post_data['CONV_ART'].mean() - pre_data['CONV_ART'].mean()) / pre_data['CONV_ART'].mean()) * 100
-            make_metric(name='Average ART', val=post_data['CONV_ART'].mean(), change=delta, d_color='inverse')
-        with CONV_FRT:
-            delta = ((post_data['CONV_FRT'].mean() - pre_data['CONV_FRT'].mean()) / pre_data['CONV_FRT'].mean()) * 100
-            make_metric(name='Average FRT', val=post_data['CONV_FRT'].mean(), change=delta, d_color='inverse')
-        with CONV_DURATION:
-            delta = ((post_data['CONV_DURATION'].mean() - pre_data['CONV_DURATION'].mean()) / pre_data['CONV_DURATION'].mean()) * 100
-            make_metric(name='Average Duration', val=post_data['CONV_DURATION'].mean(), change=delta, d_color='inverse')
-        with CSAT_SCORE:
-            delta = ((post_data['CSAT_SCORE'].mean() - pre_data['CSAT_SCORE'].mean()) / pre_data['CSAT_SCORE'].mean()) * 100
-            make_metric(name='Average CSAT', val=post_data['CSAT_SCORE'].mean(), change=delta, d_color='normal')
+        st.header('High-Low Use Analysis')
+        usage = st.slider('Usage %')
+        st.write('Median Usage = ', data['usage'].median())
+        period = st.text_input(
+            'Period for Analysis. Use M or W, the default period start is Sunday. You can specify the '
+            'day you want your grouping to start '
+            'on by using w-MON, w-TUE.', value='W')
+        high_low = qbr.usage_spliter(data, usage)
+        st.write('Date Range = ', data['date'].min(), ' - ', data['date'].max())
+        selected_teams = st.checkbox('Use Teams?')
+        if selected_teams:
+            check_period = st.checkbox('Use Period Analysis?')
+        with st.container():
+            selection = st.multiselect('Select Which Metrics To Run:',
+                                       ['CONV_ART', 'CONV_FRT', 'CONV_DURATION', 'CSAT_SCORE'])
+            st.subheader('Average Based Change Analysis')
+            High, Low = st.columns(2)
+            with High:
+                high_data = high_low[0]
+                st.subheader('High Usage Statistics')
+                st.write('High Use Conversation Count = ', high_data.shape[0])
+                if selected_teams:
+                    if check_period:
+                        high_stats = high_data.groupby(
+                            [high_data['TEAM_NAME'], pd.Grouper(key='date', freq=period)])[
+                            selection].mean().reset_index().set_index('TEAM_NAME')
+                    else:
+                        high_stats = high_data.groupby(high_data['TEAM_NAME'])[selection].mean().reset_index(
+                        ).set_index('TEAM_NAME')
+                else:
+                    high_stats = high_data.groupby(pd.Grouper(key='date', freq=period))[
+                        selection].mean().reset_index().set_index('date')
+                st.table(high_stats)
+                val_high = high_data[selection].mean().reset_index().set_index('index')
+                val_high.rename(columns={0: 'Overall Average'}, inplace=True)
+                st.table(val_high)
+            with Low:
+                low_data = high_low[1]
+                st.subheader('Low Usage Statistics')
+                st.write('Low Use Conversation Count = ', low_data.shape[0])
+                if selected_teams:
+                    low_stats = low_data.groupby(low_data['TEAM_NAME'])[selection].mean().reset_index().set_index(
+                        'TEAM_NAME')
+                else:
+                    low_stats = low_data.groupby(pd.Grouper(key='date', freq=period))[
+                        selection].mean().reset_index().set_index('date')
+                st.table(low_stats)
+                val_low = low_data[selection].mean().reset_index().set_index('index')
+                val_low.rename(columns={0: 'Overall Average'}, inplace=True)
+                st.table(val_low)
+            st.subheader('Percent Change')
+            CONV_ART, CONV_FRT, CONV_DURATION, CSAT_SCORE = st.columns(4)
+            with CONV_ART:
+                delta = ((high_data['CONV_ART'].mean() - low_data['CONV_ART'].mean()) / low_data[
+                    'CONV_ART'].mean()) * 100
+                qbr.make_metric(name='Average ART', val=high_data['CONV_ART'].mean(), change=delta, d_color='inverse')
+            with CONV_FRT:
+                delta = ((high_data['CONV_FRT'].mean() - low_data['CONV_FRT'].mean()) / low_data[
+                    'CONV_FRT'].mean()) * 100
+                qbr.make_metric(name='Average FRT', val=high_data['CONV_FRT'].mean(), change=delta, d_color='inverse')
+            with CONV_DURATION:
+                delta = ((high_data['CONV_DURATION'].mean() - low_data['CONV_DURATION'].mean()) / low_data[
+                    'CONV_DURATION'].mean()) * 100
+                qbr.make_metric(name='Average Duration', val=high_data['CONV_DURATION'].mean(), change=delta,
+                                d_color='inverse')
+            with CSAT_SCORE:
+                delta = ((high_data['CSAT_SCORE'].mean() - low_data['CSAT_SCORE'].mean()) / low_data[
+                    'CSAT_SCORE'].mean()) * 100
+                qbr.make_metric(name='Average CSAT', val=high_data['CSAT_SCORE'].mean(), change=delta, d_color='normal')
 
+if st.checkbox('Date Based Analysis'):
+    with st.container():
+        st.header('Pre-Post Analysis')
+        date_break = st.date_input('Date for Pre-Post Analysis. Make sure your period grouping matches the day of your '
+                                   'break for cleanest results. If your break is a monday, make your period start on '
+                                   'monday too', value=pd.to_datetime('2022-08-14'))
+        date_break = pd.to_datetime(date_break)
+        period = st.text_input(
+            'Period for Analysis. Use M or W, the default period start is Sunday. You can specify the '
+            'day you want your grouping to start '
+            'on by using w-MON, w-TUE.', value='W')
+        pre_post = qbr.date_splitter(data, date_break)
+        st.write('Date Range = ', data['date'].min(), ' - ', data['date'].max())
+        with st.container():
+            selection = st.multiselect('Select Which Metrics To Run:',
+                                       ['CONV_ART', 'CONV_FRT', 'CONV_DURATION', 'CSAT_SCORE'])
+            st.subheader('Average Based Change Analysis')
+            Pre, Post = st.columns(2)
+            with Pre:
+                pre_data = pre_post[0]
+                st.subheader('Pre-Date Statistics')
+                pre_stats = pre_data.groupby(pd.Grouper(key='date', freq=period))[
+                    selection].mean().reset_index().set_index(
+                    'date')
+                st.table(pre_stats)
+                val_pre = pre_data[selection].mean().reset_index().set_index('index')
+                val_pre.rename(columns={0: 'Overall Average'}, inplace=True)
+                st.table(val_pre)
+            with Post:
+                post_data = pre_post[1]
+                st.subheader('Post-Date Statistics')
+                post_stats = post_data.groupby(pd.Grouper(key='date', freq=period))[
+                    selection].mean().reset_index().set_index('date')
+                st.table(post_stats)
+                val_post = post_data[selection].mean().reset_index().set_index('index')
+                val_post.rename(columns={0: 'Overall Average'}, inplace=True)
+                st.table(val_post)
+            st.subheader('Percent Change')
+            CONV_ART, CONV_FRT, CONV_DURATION, CSAT_SCORE = st.columns(4)
+            with CONV_ART:
+                delta = ((post_data['CONV_ART'].mean() - pre_data['CONV_ART'].mean()) / pre_data[
+                    'CONV_ART'].mean()) * 100
+                qbr.make_metric(name='Average ART', val=post_data['CONV_ART'].mean(), change=delta, d_color='inverse')
+            with CONV_FRT:
+                delta = ((post_data['CONV_FRT'].mean() - pre_data['CONV_FRT'].mean()) / pre_data[
+                    'CONV_FRT'].mean()) * 100
+                qbr.make_metric(name='Average FRT', val=post_data['CONV_FRT'].mean(), change=delta, d_color='inverse')
+            with CONV_DURATION:
+                delta = ((post_data['CONV_DURATION'].mean() - pre_data['CONV_DURATION'].mean()) / pre_data[
+                    'CONV_DURATION'].mean()) * 100
+                qbr.make_metric(name='Average Duration', val=post_data['CONV_DURATION'].mean(), change=delta,
+                                d_color='inverse')
+            with CSAT_SCORE:
+                delta = ((post_data['CSAT_SCORE'].mean() - pre_data['CSAT_SCORE'].mean()) / pre_data[
+                    'CSAT_SCORE'].mean()) * 100
+                qbr.make_metric(name='Average CSAT', val=post_data['CSAT_SCORE'].mean(), change=delta, d_color='normal')
 
 with st.container():
     st.header('Sentiment Target Classification')
+    stc_file = st.file_uploader('Upload STC File')
+    stc_file = pd.read_csv(stc_file)
+    company_agent, intents_agent, order, top = st.columns(4)
+    with company_agent:
+        stc_target = st.selectbox('Sentiment Target?', ['Company', 'Agent'])
+        stc_target = stc_target.lower()
+    with intents_agent:
+        group_by = st.selectbox('Group By Agent or Intent?', ['Intents', 'Agents'])
+    with order:
+        order_it = st.selectbox('Order By?', [-2, -1, 1, 2])
+    with top:
+        cut_them = st.number_input('Number of results', value=10)
+    st.plotly_chart(qbr.diverging_sentiment(data_file=stc_file, company_or_agent=stc_target, intents_or_agents=group_by,
+                                            order_by=order_it, top=cut_them))
---
+++ qbr_functions.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.graph_objects as go
+
+
+# define some functions
+@st.cache
+def data_loader(first_data):
+    data = pd.read_csv(first_data)
+    data['date'] = pd.to_datetime(data['CONV_CREATED'])
+    data['usage'] = (data['CLICK'] / data['VIEW']) * 100
+    data['usage'].fillna(0, inplace=True)
+    return data
+
+
+@st.cache
+def make_str(text):
+    txt = str(text)
+    return txt
+
+
+@st.cache
+def date_splitter(data_set, splitter):
+    splitter1 = pd.to_datetime(splitter)
+    pre = data_set[data_set['date'] < splitter1].copy()
+    post = data_set[data_set['date'] >= splitter1].copy()
+    dset = [pre, post]
+    return dset
+
+
+@st.cache
+def usage_spliter(data_set, splitter):
+    high = data_set[data_set['usage'] >= splitter]
+    low = data_set[data_set['usage'] < splitter]
+    dset = [high, low]
+    return dset
+
+
+def red_or_green(val):
+    color = 'red' if val > 0 else 'green'
+    return f'background-color: {color}'
+
+
+def random_dates(start, end, n=10):
+    start_u = start.value // 10 ** 9
+    end_u = end.value // 10 ** 9
+
+    return pd.to_datetime(np.random.randint(start_u, end_u, n), unit='s')
+
+
+def make_metric(name, val='N/A', change='N/A', d_color='normal'):
+    st.metric(name, val, delta=change, delta_color=d_color)
+
+
+@st.cache
+def diverging_sentiment(data_file, company_or_agent='company', intents_or_agents='intents', order_by=-2, top=10):
+    # add the " " to make the print-out work
+    company_or_agent = '"' + company_or_agent + '"'
+    # convert input to column name
+    if intents_or_agents == 'Intents':
+        intents_or_agents = 'MESSAGE_LEVEL_INTENTS'
+    else:
+        intents_or_agents = 'NAME'
+    # slim the data set in prep for generating figure
+    dfagents = data_file[(data_file['SENTIMENT_TARGET'] == company_or_agent) & (data_file[intents_or_agents] != '[]')]
+    dfagents = dfagents[['RAW_SENTIMENT_VALUE', 'CONVERSATION_ID', intents_or_agents]].copy()
+    # prep pivot table for figure
+    pvt = pd.pivot_table(dfagents, index=intents_or_agents, columns='RAW_SENTIMENT_VALUE', values='CONVERSATION_ID',
+                         aggfunc='count')
+    pvt[-2] = pvt[-2] * -1
+    pvt[-1] = pvt[-1] * -1
+    pvt.fillna(0, inplace=True)
+
+    pvt.sort_values(by=order_by, inplace=True)
+    df = pvt[:top]
+    # create the figure
+    diverging = go.Figure()
+    diverging.add_trace(go.Bar(x=df[-1].values,
+                               y=df.index,
+                               orientation='h',
+                               name=-1,
+                               customdata=df[-1],
+                               hovertemplate="%{y}: %{customdata}",
+                               marker_color='#f86388',
+                               text=df[-1] * -1))
+
+    diverging.add_trace(go.Bar(x=df[-2].values,
+                               y=df.index,
+                               orientation='h',
+                               name=-2,
+                               customdata=df[-2],
+                               hovertemplate="%{y}: %{customdata}",
+                               marker_color='#c42f55',
+                               text=df[-2] * -1))
+    diverging.add_trace(go.Bar(x=df[1],
+                               y=df.index,
+                               orientation='h',
+                               name=1,
+                               hovertemplate="%{y}: %{x}",
+                               marker_color='#09aea1',
+                               text=df[1]))
+    diverging.add_trace(go.Bar(x=df[2],
+                               y=df.index,
+                               orientation='h',
+                               name=2,
+                               hovertemplate="%{y}: %{x}",
+                               marker_color='#077f74',
+                               text=df[2]))
+    diverging.update_layout(barmode='relative',
+                            height=800,
+                            width=1400,
+                            yaxis_autorange='reversed',
+                            bargap=0.3,
+                            legend_orientation='v',
+                            legend_title_text='Message Sentiment Score',
+                            legend_x=1, legend_y=0,
+                            plot_bgcolor='#f5f5f5',
+                            paper_bgcolor='#fff')
+
+    return diverging
